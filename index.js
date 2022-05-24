6dffabeaaa0271d4cc3c1e4dce128ec50597db1d
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-05-24 17:14:27 +0100

    commit tree f6f09fab6558cbbe9d81adfa4a5ee9fa7672d10b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+/*findMatching= function(drivers, name){
+    const newArray = [];
+    for(let driver of drivers){
+        if (driver.toUpperCase() === name.toUpperCase()){
+            newArray.push(driver);
+        }
+    }
+    return newArray
+}*/
+
+function findMatching(drivers, name){
+    
+   const newArray = drivers.filter((driver)=>driver.toUpperCase() === name.toUpperCase())
+
+    return newArray;
+
+}
+
+function fuzzyMatch(drivers, string){
+    const secondArray = drivers.filter((driver)=>driver.substring(0,2) === string)
+    return secondArray;
+
+}
+
+function matchName(drivers, string){
+    const thirdArray = drivers.filter((driver)=> driver.name === string);
+    return thirdArray;
+}
